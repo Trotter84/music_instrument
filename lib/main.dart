37a8217522261ff4cc12670d5a21f00a1d7e264a
Daniel Trotter <flare84@gmail.com>
2021-09-28 00:38:43 -0600

    made function to simplify repeats

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(const InstrumentApp());
-}
+void main() => runApp(const InstrumentApp());
+
 
 class InstrumentApp extends StatelessWidget {
   const InstrumentApp({Key? key}) : super(key: key);
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             children: [
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
-              child: Container(
-                color: Colors.red,
-                width: 100.0,
-                height: 50.0,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note2.wav');
-                },
-                child: Container(
-                  color: Colors.orange,
-                  width: 100.0,
-                  height: 50.0,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note3.wav');
-                },
-                child: Container(
-                  color: Colors.yellow,
-                  width: 100.0,
-                  height: 50.0,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note4.wav');
-                },
-                child: Container(
-                  color: Colors.greenAccent,
-                  width: 100.0,
-                  height: 50.0,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note5.wav');
-                },
-                child: Container(
-                  color: Colors.green,
-                  width: 100.0,
-                  height: 50.0,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note6.wav');
-                },
-                child: Container(
-                  color: Colors.blue,
-                  width: 100.0,
-                  height: 50.0,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note7.wav');
-                },
-                child: Container(
-                  color: Colors.purple,
-                  width: 100.0,
-                  height: 50.0,
-                ),
-              ),
-
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
             ],
           ),
         ),
