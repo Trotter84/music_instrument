59294505d09fd24a6bfcd46ad45043ce0648f89f
Daniel Trotter <flare84@gmail.com>
2021-09-27 19:44:08 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const InstrumentApp());
+}
+
+class InstrumentApp extends StatelessWidget {
+  const InstrumentApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            color: Colors.blue.shade500,
+          ),
+        ),
+      ),
+    );
+  }
+}
