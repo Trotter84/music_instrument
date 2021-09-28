0124b07fd300342b3bcef310613b9e14ad144430
Daniel Trotter <flare84@gmail.com>
2021-09-27 21:22:48 -0600

    seven buttons added and working

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,87 @@ class InstrumentApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+              child: Container(
+                color: Colors.red,
+                width: 100.0,
+                height: 50.0,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Container(
+                  color: Colors.orange,
+                  width: 100.0,
+                  height: 50.0,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                child: Container(
+                  color: Colors.yellow,
+                  width: 100.0,
+                  height: 50.0,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+                child: Container(
+                  color: Colors.greenAccent,
+                  width: 100.0,
+                  height: 50.0,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+                child: Container(
+                  color: Colors.green,
+                  width: 100.0,
+                  height: 50.0,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+                child: Container(
+                  color: Colors.blue,
+                  width: 100.0,
+                  height: 50.0,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+                child: Container(
+                  color: Colors.purple,
+                  width: 100.0,
+                  height: 50.0,
+                ),
+              ),
+
+            ],
           ),
         ),
       ),
